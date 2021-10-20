15b1be21693abfc4c11058c07e1224ec17b81831
Man E <mannyportillo77@gmail.com>
2021-10-20 17:14:33 -0600

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/libro-seached',
-  // {
-  //   useNewUrlParser: true, 
-  //   useUnifiedTopology: true,
-  //   useCreateIndex: true,
-  //   useFindAndModify: false
-  // }
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
 );
 
 module.exports = mongoose.connection;
